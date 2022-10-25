775e4bac27dfbe3c34bc7f38275070e0940690e4
Tobi Balogun <tobi.balo@gmail.com>
2022-10-25 23:38:49 +0100

    new: connect application to database

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const CONFIG = require('./config/config')
 const express = require('express')
 
 const app = express()
+
+// connect to db
+require('./middleware/db')(CONFIG.DBURI)
+
+// parse information from request
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 module.exports = app
