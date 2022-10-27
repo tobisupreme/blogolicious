9084c2f518695d003a02b3ebbfeac0962e4d6d11
Tobi Balogun <tobi.balo@gmail.com>
2022-10-27 01:25:35 +0100

    fix: modify User model to pass test

--- models/User.js
+++ models/User.js
@@ -19,6 +19,19 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+UserSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.password
+  },
 })
 
 module.exports = mongoose.model('User', UserSchema)
