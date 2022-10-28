a26ef259109f13049c2e1bd189fdb71355fa64a2
Tobi Balogun <tobi.balo@gmail.com>
2022-10-28 06:55:12 +0100

    fix: return appropriate error response when password is missing

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,6 +2,12 @@
  * Error handler middleware
  */
 module.exports = (error, req, res, next) => {
+  if (error.message === 'data and hash arguments required') {
+    return res.status(403).json({
+      error: 'please provide password',
+    })
+  }
+
   res.status(400).json({
     error: error.message,
   })
