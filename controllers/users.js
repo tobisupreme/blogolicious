583f9117e89edc172f8221e48e5a221ad4d8f7b2
Tobi Balogun <tobi.balo@gmail.com>
2022-10-28 06:54:43 +0100

    fix: move password hash function to database

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,16 @@
 const User = require('../models/User')
-const bcrypt = require('bcrypt')
 
 const createUser = async (req, res, next) => {
   try {
     // grab details from the request
     const { firstName, lastName, username, email, password } = req.body
-    // hash user password
-    const hashedPassword = await bcrypt.hash(password, 10)
     // create user object
     const newUser = new User({
       firstName,
       lastName,
       username,
       email,
-      password: hashedPassword,
+      password,
     })
     // save to database
     const createdUser = await newUser.save()
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
   firstName: {
@@ -24,6 +25,38 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
 })
+
+// encrypt password before saving document
+UserSchema.pre('save', function (next) {
+  let user = this
+
+  // do nothing if the password is not modified
+  if (!user.isModified('password')) return next()
+
+  // hash the password using our new salt
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    if (err) return next(err)
+
+    //override the clear text password with the hashed one
+    user.password = hash
+    next()
+  })
+})
+
+// Compare user inputted password with password in the database
+UserSchema.methods.passwordIsValid = function (password) {
+  // get password from the database
+  const passwordHash = this.password
+  return new Promise((resolve, reject) => {
+    // compare the password coming from the user with the hash password in the database
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(same)
+    })
+  })
+}
 
 UserSchema.set('toJSON', {
   transform: (document, returnedObject) => {
