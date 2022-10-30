9f4ef5cfbcf0f374a76b5545b92dc9e664ebed90
Tobi Balogun <tobi.balo@gmail.com>
2022-10-30 14:58:52 +0100

    new: add tests for adding new blogs

---
+++ tests/blog.test.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose')
+const app = require('../app')
+const supertest = require('supertest')
+const api = supertest(app)
+const User = require('../models/User')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await User.deleteMany({})
+  for (let i = 5; i > 0; i--) {
+    await User.create(helper.createUserObject(i))
+  }
+})
+
+describe('Creating a blog', () => {
+  let token
+
+  const login = async (username) => {
+    const response = await api
+      .post('/api/login')
+      .send({
+        username,
+        password: 'Password0!',
+      })
+
+    token = response.body
+  }
+
+  it('should work with valid token', async () => {
+    const user = 'user3'
+    await login(user)
+
+    const blogsBefore = await helper.articlesInDb()
+
+    const response = await api
+      .set('Authorization', `Bearer ${token}`)
+      .post('/api/blog')
+      .send(helper.articleObject(`Article by ${user}`))
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.data).toHaveProperty('title')
+    expect(response.body.data).toHaveProperty('description')
+    expect(response.body.data).toHaveProperty('tags')
+    expect(response.body.data).toHaveProperty('author')
+    expect(response.body.data).toHaveProperty('createdAt')
+    expect(response.body.data).toHaveProperty('updatedAt')
+    expect(response.body.data).toHaveProperty('read_count')
+    expect(response.body.data).toHaveProperty('reading_time')
+    expect(response.body.data).toHaveProperty('body')
+    expect(response.body.data).toHaveProperty('state')
+    expect(response.body.data.state).toBe('draft')
+
+    const blogsAfter = await helper.articlesInDb()
+    expect(blogsBefore.length).toBe(blogsAfter.length - 1)
+
+  })
+
+  it('should return an error if no valid tokens are provided', async() => {
+    const blogsBefore = await helper.articlesInDb()
+    const response = await api
+      .post('/api/blog')
+      .send(helper.articleObject('Article by no registered user'))
+      .expect(403)
+
+    expect(response.body.status).toBe(false)
+
+    const blogsAfter = await helper.articlesInDb()
+    expect(blogsBefore.length).toBe(blogsAfter.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,10 +1,38 @@
 const User = require('../models/User')
+const Blog = require('../models/Article')
 
-const usersInDb = async() => {
+const usersInDb = async () => {
   const users = await User.find({})
-  return users.map(user => user.toJSON())
+  return users.map((user) => user.toJSON())
+}
+
+const articlesInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map((blog) => blog.toJSON())
+}
+
+const createUserObject = (n) => {
+  return {
+    firstName: 'user',
+    lastName: `${n}`,
+    username: `user${n}`,
+    email: `user${n}@mail.com`,
+    password: 'Password0!',
+  }
+}
+
+const articleObject = (title) => {
+  return {
+    title,
+    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat, est!',
+    tags: ['lorem', 'royal'],
+    body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus nam, eum a quod eius quasi modi tempore sed, pariatur totam amet corporis atque dolorum cupiditate! Nulla culpa modi reprehenderit deserunt laboriosam voluptatem incidunt quidem! Quibusdam, neque in eveniet temporibus id reiciendis quidem ratione voluptas accusamus voluptatibus, consectetur, vero odio deserunt tenetur provident non? Quasi necessitatibus minus veniam autem quos, numquam dolore quae nemo commodi. Tempora maiores dolore laborum veniam sed fuga eveniet facilis tenetur, possimus at distinctio iste a earum officiis laudantium nulla id necessitatibus amet et mollitia? Obcaecati doloribus et a nesciunt atque excepturi accusamus nostrum similique rem, optio voluptas ut tempora! Laudantium excepturi deleniti animi dolorum illo sequi, accusantium eius id quia, inventore dignissimos maiores? Voluptatem quasi exercitationem iusto autem possimus assumenda labore reprehenderit sequi nostrum.',
+  }
 }
 
 module.exports = {
-  usersInDb
+  usersInDb,
+  createUserObject,
+  articleObject,
+  articlesInDb,
 }
